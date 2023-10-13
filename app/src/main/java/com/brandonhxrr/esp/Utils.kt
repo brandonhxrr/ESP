4a72370575bfdb800658923fe5f220a70d72f482
Brandon Herrera <brandondh769@gmail.com>
2023-10-12 19:26:28 -0600

    Add utils file with isIPAddress function

---
+++ app/src/main/java/com/brandonhxrr/esp/Utils.kt
@@ -0,0 +1,10 @@
+package com.brandonhxrr.esp
+
+fun isIPAddress(text: String): Boolean {
+    val patronIP = ("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$").toRegex()
+
+    return patronIP.matches(text)
+}
